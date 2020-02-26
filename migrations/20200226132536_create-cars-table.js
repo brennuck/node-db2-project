ec34be8bd5f52a81f8836e3e8787a454cb2e4da4
Brennon Nuckols <brennuck@gmail.com>
2020-02-26 13:51:23 -0700

    migrations complete

---
+++ migrations/20200226132536_create-cars-table.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments();
+
+    tbl.integer('VIN', 32).notNullable().index();
+
+    tbl.string('make', 128).notNullable()
+      
+    tbl.string('model', 128).notNullable()
+
+    tbl.integer('mileage').notNullable()
+
+    tbl.boolean('automatic').defaultTo(true);
+
+    tbl.boolean('clean').defaultTo(true);
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("cars");
+};
